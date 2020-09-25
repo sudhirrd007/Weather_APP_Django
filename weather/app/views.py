830286080d730655e3b9c40831e7bdc62e363643
sudhirrd007 <sudhirrd007@gmail.com>
2020-09-25 16:23:51 +0530

    add API interface into views

--- weather/app/views.py
+++ weather/app/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+import requests
+# get your API KRY and changed with below key
+api_key = '243be12ced24cc2a61ec4f13e258d2a4'
 
 def index(request):
-    return render(request, 'app/index.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='+api_key
+    city = "Las Vegas"
+    city_weather = requests.get(url.format(city)).json()
+
+    weather = {
+        "city": city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon']
+    }
+
+    context = {"weather":weather}
+
+    return render(request, 'app/index.html', context=context)
