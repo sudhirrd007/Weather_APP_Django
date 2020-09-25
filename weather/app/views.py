75420760a0fd0bab6fa421110b64b00fa74db10a
sudhirrd007 <sudhirrd007@gmail.com>
2020-09-25 17:07:03 +0530

    link CityForm to views.py

--- weather/app/views.py
+++ weather/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 from app.models import City
+from app.forms import CityForm
 
 # get your API KRY and changed with below key
 api_key = '243be12ced24cc2a61ec4f13e258d2a4'
@@ -8,6 +9,12 @@ api_key = '243be12ced24cc2a61ec4f13e258d2a4'
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='+api_key
     cities = City.objects.all() # return all the cities in the DB
+
+    if(request.method == "POST"): # Only true if form is submitted
+        form = CityForm(request.POST)  # add actual request data to form for processing
+        form.save() # will validate and save if validate
+
+    form = CityForm()
 
     weather_data = []
 
@@ -21,6 +28,6 @@ def index(request):
         }
         weather_data.append(weather)
 
-    context = {"weather_data" : weather_data}
+    context = {"weather_data" : weather_data, "form": form}
 
     return render(request, 'app/index.html', context=context)
