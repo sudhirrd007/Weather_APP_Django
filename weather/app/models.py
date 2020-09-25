b64168b729dcba74012c1abf47314155495cb8ef
sudhirrd007 <sudhirrd007@gmail.com>
2020-09-25 16:33:43 +0530

    add a City model

--- weather/app/models.py
+++ weather/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self): # show the actual city name on the deskboard
+        return self.name
+    
+    class Meta(): # show plural of city as cities insetead of citys
+        verbose_name_plural = 'cities'
