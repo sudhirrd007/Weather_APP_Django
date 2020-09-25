911c3d686684f134d7ec5774faa7320cde270384
sudhirrd007 <sudhirrd007@gmail.com>
2020-09-25 16:43:40 +0530

    add multiple sity into index.py from DB

--- weather/app/admin.py
+++ weather/app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from app import models
+
+admin.site.register(models.City)
--- weather/app/views.py
+++ weather/app/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
 import requests
+from app.models import City
+
 # get your API KRY and changed with below key
 api_key = '243be12ced24cc2a61ec4f13e258d2a4'
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='+api_key
-    city = "Las Vegas"
-    city_weather = requests.get(url.format(city)).json()
+    cities = City.objects.all() # return all the cities in the DB
 
-    weather = {
-        "city": city,
-        'temperature' : city_weather['main']['temp'],
-        'description' : city_weather['weather'][0]['description'],
-        'icon' : city_weather['weather'][0]['icon']
-    }
+    weather_data = []
 
-    context = {"weather":weather}
+    for city in cities:
+        city_weather = requests.get(url.format(city)).json()
+        weather = {
+            "city": city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+        weather_data.append(weather)
+
+    context = {"weather_data" : weather_data}
 
     return render(request, 'app/index.html', context=context)
